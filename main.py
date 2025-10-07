bb050582e7f19f12f797861709ece748fd6c3dac
Hassan-Abd-El-Aziz <hassanabdelaziz940@gmail.com>
2025-10-07 14:40:37 +0300

    advanced example

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-name=10
-if name !=str:
-    raise Exception("not string")
-print("finsh") #not working
-
-n="zizo"
-if n != int:
-    raise ValueError("not a number")
-print("code not printing")
+my_file=None
+my_tries=5
+while my_tries >0:
+    try:
+        print(f"you have {my_tries} tries left")
+        files=(input("enter your path file => :").strip())
+        my_file=open(files,"r")
+        print(my_file.read())
+        break
+    except FileNotFoundError:
+        print("file not found ")
+        my_tries -=1
+    except:
+        print("error happens")
+    finally:
+        my_file.close()
+        print("file closed")
+else:
+    print("end tries")
