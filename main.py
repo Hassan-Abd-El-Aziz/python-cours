803cc2184d1d56d3012c3c870cf50a2b0ac79042
Hassan-Abd-El-Aziz <hassanabdelaziz940@gmail.com>
2025-10-15 14:08:46 +0300

    classMethod and staticMethod

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
-import re
-my_string="I Love pythone"
-search_string=re.split(r"\s",my_string,1)
-print(search_string)
+class Member:
+    not_allawed_names=["hima","figo"]
+    users_num=0
 
-strng_two="I_Love-you_pythone-and_A-js"
-search_two=re.split(r"-|_",strng_two)
-print(search_two)
 
-for count,word in enumerate(search_two,1):
-    if len(word)==1:
-        continue
-    print(f"word num: {count} is {word}")
+    @classmethod 
+    def show_user_count(cls):
+        return f"number of users {cls.users_num}"
+    @staticmethod
+    def say_good():
+        return 'good mornning'
 
-print("*" *70)
 
-string_three="I_love_you_somuch"
-search_three=re.sub(r"_"," $ ",string_three)
+    def __init__(self,firstName,lastName):
+        self.fname=firstName
+        self.lname=lastName
+        Member.users_num +=1
+    def say_hello(self):
+        return f"Hello {self.fname}"
+    def delete_user(self):
+        Member.users_num -=1
+        print(f"user {self.fname} is deleted")
 
-print(search_three)
+
+uOne=Member("Hassan","zizo")
+uTwo=Member("ahmed","hassan")
+uthree=Member("hima","figo")
+
+
+print(Member.users_num)
+
+print(uOne.say_hello())      
+print(uTwo.say_hello())      
+print(uthree.say_hello())    
+
+print(Member.users_num)
+print(Member.show_user_count())
+print(Member.say_good())
