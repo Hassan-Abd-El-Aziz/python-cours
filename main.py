6045cbe528766e559f8ba9aab4df8bfdfc6486bd
Hassan-Abd-El-Aziz <hassanabdelaziz940@gmail.com>
2025-10-26 17:10:40 +0300

    create DB

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from abc import ABCMeta,abstractmethod
+import sqlite3
 
-class programin(metaclass=ABCMeta):
-    @abstractmethod
-    def has_oop(self):
-        pass
-    def goo(self):
-        return "good"
-class python(programin):
-    def has_oop(self):
-        return "Yes"
+def get_data():
+    try:
+        db=sqlite3.connect("app.db")
+        print("connected to database")
+        cr=db.cursor()
+        cr.execute("CREATE TABLE if not exists users(name text, salary integer, title text)")
+        cr.execute("INSERT INTO users(name,salary,title) values ('hassan',5000,'enginner')")
+        cr.execute("INSERT INTO users(name,salary,title) values ('hassan',5000,'enginner')")
+        db.commit()
+    except sqlite3.DatabaseError as err:
+        print(f" data error {err}")
+    finally:
+        print("database closed")
+        db.close()
 
 
-one=python()
-print(one.has_oop())
-print(one.goo())
+
+
+
+
+get_data()
