81398dba7d8937ae9242336894462ab3de6b4b32
Hassan-Abd-El-Aziz <hassanabdelaziz940@gmail.com>
2025-10-27 11:53:59 +0300

    strings methods

--- main.py
+++ main.py
@@ -1,23 +1,111 @@
-import sqlite3
+h="I love you Hassan"
 
-def get_data():
-    try:
-        db=sqlite3.connect("app.db")
-        print("connected to database")
-        cr=db.cursor()
-        cr.execute("CREATE TABLE if not exists users(name text, salary integer, title text)")
-        cr.execute("INSERT INTO users(name,salary,title) values ('hassan',5000,'enginner')")
-        cr.execute("INSERT INTO users(name,salary,title) values ('hassan',5000,'enginner')")
-        db.commit()
-    except sqlite3.DatabaseError as err:
-        print(f" data error {err}")
-    finally:
-        print("database closed")
-        db.close()
+print(h.index("n"))
+print(h.startswith("H",11))
+print(h.endswith("n",16))
+print(h.swapcase())
+
+print(h.split())
+print(h.rsplit(" ",2))
+
+print(h.center(50,"#"))
+print(h.find("ss"))
+
+print("*" *50)
+
+a,b,c,z="1","11","111","1111"
+print(a.zfill(4))
+print(b.zfill(4))
+print(a.zfill(4))
+print(a.zfill(4))
+
+print("*" *50)
+
+name="zizo"
+
+print(name.ljust(10,"@"))
+print(name.rjust(10,"#"))
 
 
+e='''hi hassan,
+ilove python
+and i love js
+'''
+print(e.splitlines())
+s=e.splitlines()
+for a in s :
+    print(a)
+
+print("*" *50)
+
+txt="heloo\thassan\tilove\tyou"
+print(txt)
+print(txt.expandtabs(2))
+
+print("*" *50)
+
+tst="I Need 5G"
+print(tst.istitle())#true
+tst="I need 5g"
+print(tst.istitle())#false
+print("*" *50)
+
+sps=" "
+print(sps.isspace())
+print("*" *50)
 
 
+l="ilove"
+print(l.islower())
+print(l.isalpha())
 
 
-get_data()
+idn="hass_n"
+idn2="hass-n"
+print(idn.isidentifier())
+print(idn2.isidentifier())
+
+alln="h33loz1zo"
+
+print(alln.isalnum())
+
+
+var="one two one two three two three one "
+
+print(var.replace("one","1"))
+print(var.replace("two","2",1))
+print(var.replace("three","3",2))
+
+lst=["one","two","three","four"]
+print(lst)
+print("-".join(lst))
+print(" ".join(lst))
+print("*" *50)
+
+nam="hassan"
+age=62
+rate=1.5458
+
+# old_formating
+print("wellcome %s" % nam)
+print("Hello %s your age is %d" % (nam,age))
+print("hi %.3s age is %d" % (nam,age))
+print("hi %.3s age is %d rate is %.2f" % (nam,age,rate))
+
+# new_formating
+print("wellcom {:s}".format(nam))
+print("Hello {:s} your age is {:d}".format(nam,age))
+print("hi {:.3s} your age is {:d} rate is {:.3f}".format(nam,age,rate))
+
+
+format_mony=15454654654564
+print("your mony in bank is {:,d}" .format(format_mony))
+
+a,b,c="one","two","three"
+print("{2:s}, {1:s} , {0:s}" .format(a,b,c))
+
+# last New Formating
+skill=["Html","css","js"]
+
+for k,s in enumerate(skill):
+    print(f"skill {k + 1} => is {s.capitalize()}")
