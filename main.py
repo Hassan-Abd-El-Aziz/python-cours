5fe11d9673c662ffb471daec011b7c3ee80e9e70
Hassan-Abd-El-Aziz <hassanabdelaziz940@gmail.com>
2025-11-05 17:02:35 +0200

    tuple- user input - while

--- main.py
+++ main.py
@@ -1,85 +1,156 @@
-#Dictionary
+a="hassan"
+d=["one",2,"two"]
+c=("a","b","c")
+b={1,2,3}
+e={"a":"one","b":"two"}
+q=100
 
-user={"name":"hassan","salary":50000,"avilabel":True}
+print(list(a))
+print(list(b))
+print(list(c))
+print(list(e))
+# print(list(q))#error
 
+print(tuple(a))
+print(tuple(b))
+print(tuple(e))
 
-print(user.keys())
-print(user.values())
-print(user.get("name"))
-print(user.get("salary"))
+# user input
+print("*" * 50)
+f_name=input("enter your first name :")
+l_name=input("enter your last name :")
+age=input("enter your age :")
 
-if user.get("avilabel")==True:
-    print(f"well come {user['name']}")
+print(f"Hello {f_name} {l_name[0]} your age is {age}")
 
+# *************************************************************
+name=input("enter your name").strip().capitalize()
+email=input("enter mail ").strip()
 
-skills={"one":{"name":"html","progres":"90%"}}
-skills2={"two":{"name":"css","progres":"85%"}}
-print("*" *70)
+mailname=email[:email.index("@")]
+domin=email[email.index("@")+1:]
+print(f"hi {name} name in mail is :{mailname} your domain is: {domin}")
 
-print(skills)
-print(skills.get("one").get("name"))
-print(skills['one']['progres'])
+# -----------------------------------------------
+age=int(input("enter your age :").strip())
 
-allskills={"langOne":skills,"langTwo":skills2}
+month=age*12
+week=month*4
+days=week*365
 
-print(allskills)
+print(f"age in month is {month}")
+print(f"age in week is {week}")
+print(f"age in days is {days:,}")
 
-#methods
-allskills.clear()
-print(allskills)
-allskills.update({"one":"html"})
-allskills['one']="css"
-print(allskills)
-allskills.update({"two":"html"})
-print(allskills)
+# -------------------------------------------------
+name="hassan"
+cuntry=input("enter your cuntry : ").strip()
+price=1500
+if cuntry=="ksa":
+    print(f"Hello {name} from {cuntry} your price is {price-50}")
+elif cuntry=="egypt":
+    print(f"Hello {name} from {cuntry} your price is {price-500}")
+elif cuntry=="Kwit":
+    print(f"Hello {name} from {cuntry} your price is {price-100}")
+else:
+    print(f"Hello {name} from {cuntry} your price is {price-10}")
+    
 
-call=allskills.copy()
-call.update({"three":"javascript"})
-allskills.clear()
-print(allskills)
-print(call)
+trnary if
+name="hassan"
+age=19
+movierate=18
+print(f"welcome {name}" if age >movierate else "out of rate")
 
+# **********************
+age=int(input("enter your age :"))
+month=age*12
+week=month*4
+days=week*365
+inp=input("enter what do you want : m,w,d")
 
-print(call.popitem())
+print(f"heloo zizo {month}" if inp == "m" else "error"   )
+print(f"heloo zizo {week}" if inp == "w" else "error"   )
+print(f"heloo zizo {days}" if inp == "d" else "error"   )
+# ********************
 
+my_frinds=["hassan","zizo","mido"]
 
-print("*" *50)
-a={"one":None,"t":15}
-print(a)
-print(a.setdefault("zizo",55))
-print(a)
+print("zizo" in my_frinds)
 
-print("*" *50)
+name="mido"
+print("d" in name)
 
-viw={"one":"lst1","two":"lst2"}
-lls=viw.items()
-viw['three']="lst3"
-print(lls)
-print(viw)
+# /////////////////\\\\\\\\\\\\\\\\\
+cuntries=["Eg","ks","sa"]
+cun=input("enter your cuntry : ")
+if cun in cuntries:
+    print(f"helooo figo")
+else:
+    print("goooo")
 
-for ls in lls:
-    print(f"key: {ls[0]} value is {ls[1]}")
+admins=["Hassan","Hhmed"]
+login_name=input("enter your name").strip().capitalize()
 
-va=("one","two","three")
-b="x"
-print(dict.fromkeys(va,b))
+if login_name in admins:
+    delete_update=input("inter what do you want delete, or update or d or m").capitalize()
+    if delete_update =="Update" or delete_update=="U":
+        newval=input("enter new value").strip().capitalize()
+        admins[admins.index(login_name)]=newval
+        print(admins)
+    elif delete_update =="Delete" or delete_update=="D":
+        admins.remove(login_name)
+        print(admins)
+    else:
+        print("wrong option")
+else:
+    print("your are not admin add you")
+    newadmin=input("select y,n")
+    if newadmin =="y":
+        admins.append(login_name)
+        print(admins)
+    else:
+        print("your are not added")
 
-print(bool(''))
-print(bool('1'))
+# ******************************
+a=["a","b","c","d"]
+s=0
+while s<len(a):
+    print(f"number {str(s+1) } {a[s]}")
+    s +=1
+else:
+    print("all printed")
 
+print("*" *100)
 
-age=10
-cun="EG"
-rank=10
+my_webs=[]
+maximum=5
+while maximum > 0:
+    web=input("enter your web").strip().lower()
+    my_webs.append(web)
+    maximum -=1
+    print(f"https:// {my_webs}")
+    print(f"you have {maximum} tries left")
+else:
+    print("full")
 
-print(cun=="EG" and age >5 and rank >3)
-print(cun=="EG" and age >5 and rank >11)
+print("*" *100)
 
-print(cun=="EG" or age >5 or rank >3)
-print(cun=="EG" or age >15 or rank >11)
+tries=4
+mainpqssword="zizo"
+password=input("enter your passwersd : ")
 
-print(not age >50)
+while password != mainpqssword:
+    tries -=1
+    print(f"wrong password you have {'last' if tries==0 else tries} left")
+    password=input("enter your passwersd : ")
 
-x=10
-x+=10
-print(str(x))
+    if tries==0:
+        print("all tries is finshed")
+        break
+else:
+    print("correct passward")
+
+print("*" *100)
+for r in range(1,11):
+    print(f"number {r}")
